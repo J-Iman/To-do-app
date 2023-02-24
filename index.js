1aefaad1b6a8fcfebbf53a440ea7cadb75c3e48d
J-Iman <eng.jallali@gmail.com>
2023-02-24 11:14:13 -0800

    mobile version and button functionality is added

--- index.js
+++ index.js
@@ -15,19 +15,44 @@ addButton.addEventListener('click', function(){
     else{
         inputData.style.border = "2px solid white"
 
+        var insidetodocontainer = document.createElement('div')
+        insidetodocontainer.id = "insidetodocontainer" // for jquery purpose we gave it an id to access it later
+        var leftContainer = document.createElement('div')
+        var rightContainer = document.createElement('div')
+
         var paragraph = document.createElement('p')
+        var removeButton = document.createElement('button')
+        var crossButton = document.createElement('button')
+
+        removeButton.id = 'removetask'
+        removeButton.innerHTML = 'Remove'
+        removeButton.classList.add('remove-button-style')
+
+        crossButton.id = 'crosstask'
+        crossButton.innerHTML = 'Done'
+        crossButton.classList.add('cross-button-style')
+
         paragraph.innerText = inputData.value;
         paragraph.classList.add("applyonparagraph")
-        todoContainer.appendChild(paragraph)
-    
-        //single click on P tag action
-        paragraph.addEventListener('click', function(){
-            paragraph.style.backgroundColor = 'white'
+        
+        // layout style on Main bottom container
+        insidetodocontainer.classList.add('inside-to-do-container-style')
+        
+        todoContainer.appendChild(insidetodocontainer)
+        insidetodocontainer.append(leftContainer)
+        insidetodocontainer.append(rightContainer)
+
+        leftContainer.append(paragraph)
+        rightContainer.append(crossButton)
+        rightContainer.append(removeButton)
+
+
+        removeButton.addEventListener('click', function(){
+            todoContainer.removeChild(insidetodocontainer)
         })
-    
-        // Double click on P tag Action 
-        paragraph.addEventListener('dblclick', function(){
-            todoContainer.removeChild(paragraph)
+
+        crossButton.addEventListener('click', function(){
+            paragraph.style.textDecoration = "line-through"
         })
 
         inputData.value = ""
